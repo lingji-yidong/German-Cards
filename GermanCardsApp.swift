69eb539a8b903a37989bcac80f9cff8b33db1b66
lingji-yidong <lingji-yidong@proton.me>
2026-05-21 14:57:29 +0000

    fix: package mac release and polish icon

--- GermanCardsApp.swift
+++ GermanCardsApp.swift
@@ -12,6 +12,12 @@ struct GermanCardsApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                #if targetEnvironment(macCatalyst)
+                .frame(minWidth: 390, idealWidth: 430, maxWidth: 520, minHeight: 760, idealHeight: 860)
+                #endif
         }
+        #if targetEnvironment(macCatalyst)
+        .defaultSize(width: 430, height: 860)
+        #endif
     }
 }
